4ae4e4d3a1f4452f0f853e7e14c291dab529d06c
Adam-Arezza <arezza.adam@gmail.com>
2019-02-11 09:42:52 -0500

    completed basic testing, displaying HMI/PLC information

--- server/index.js
+++ server/index.js
@@ -3,55 +3,64 @@ const cors = require('cors')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
-var mqtt = require('mqtt')
+const mqtt = require('mqtt')
 
 //The mqtt broker IP and port to connect to
 var hmi = {
-  host: '192.168.2.162',
-  port: 1883
+    host: '192.168.2.162',
+    port: 1883
 }
 
 //initializing a connection to the HMI Mqtt broker
 var client = mqtt.connect(hmi)
-
-//creating a connection between the express server and the HMI mqtt broker
-//io.emit allows an event and data to be sent to the client through the socket connection
-client.on('connect', function(){
-    if(client.connected){
-        io.emit('clientConn', [true])
-    }
-})
-
-//subscribing the server to the hmi topics
-client.subscribe({
-    'HMITopics' : 0,
-    'startBtn' : 0
-}, {}, function(err, granted){
-    if(err){
-        console.log(err)
-    }
-    if(granted){
-        // console.log(granted)
-    }
-} )
-
-//on a message event (sent from the hmi to the server)
-//the data (message) is relayed to the browser (client) via io.emit
-client.on('message', function(topic,message,packet){
-    // console.log(topic)
-    var data = JSON.parse(message.toString())
-    // console.log(data.d)
-    if(data.d.startLine){
-        io.emit('startBtn', data.d.startLine)
-        //console.log(data.d.startLine)
-    }
-})
+var subscribedTopics = []
 
 app.use(cors())
 app.use(express.static('./dist'))
 
-io.on('connection', () => {
-    console.log('Listening on port 3000!')
+function subToTopics(topics){
+    topics.forEach(topic => {
+        client.subscribe(topic)
+    })
+}
+
+function startUpConnection(){
+    if (client.connected) {
+        io.emit('mqttConn', [true])
+        client.subscribe("#")
+        client.unsubscribe("#")
+    }
+}
+
+io.on('connection', (socket) => {
+    io.emit('browserConn', [true])
+    socket.on('brokerConn', () => {
+        startUpConnection()
+    })
+    socket.on('subTopic', topic => {
+        subscribedTopics.push(topic)
+        subToTopics(subscribedTopics)
+    })
+    socket.on('reConnect', function(){
+        client.reconnect()
+        startUpConnection()
+    })
+})
+
+//on a message event (sent from the hmi to the server)
+//the data (message) is relayed to the browser (client) via io.emit
+client.on('message', function (topic, message) {
+    var data = JSON.parse(message.toString())
+    if(subscribedTopics.includes(topic)){
+        if(data.d["Part Length"]){
+            data.d["Part Length"] = (data.d["Part Length"] * 0.01).toFixed(2)
+        }
+        io.emit('topicData', data.d)
+    }
+    if(data.d.topics){
+        io.emit('topicList', data.d.topics)
+    }
 })
 server.listen(3000)
+
 
--- src/components/mqtt.vue
+++ src/components/mqtt.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="hello">
-    <p>Connection Status: {{connectionStatus}}</p>
-    <p>Start button pushed: {{startBtnStatus}}</p>
+  <div class="machineMonitor">
+    <div id="connections">
+      <p v-bind:class="[client_server ? 'isConnected' : 'notConnected']">Client-Server Socket connection: {{client_server}}</p>
+      <p v-bind:class="[mqttConnection ? 'isConnected' : 'notConnected']">Mqtt Broker Connection Status: {{mqttConnection}}</p>
+      <br>
+      <button @click="reConnect">Re-Connect</button>
+    </div>
+    <div>
+      <button @click="hidden = !hidden">Toggle list</button>
+      <div v-if="topicList">
+        <ul v-if="hidden == false">
+        <li v-for="(topic,index) in topicList" :key="index">
+          {{topic.topic}}
+          <button v-if="subscribed[index] != topic.topic" @click="subTopic(topic.topic)">Subscribe to topic</button>
+          <button v-if="subscribed[index] == topic.topic" @click="unsubTopic(topic.topic)">Unsubscribe from topic</button>
+        </li>
+      </ul>
+      </div>
+      <div class="machineParams" v-if="topicParams">
+        <ul>
+          <li v-for="(name, index) in topicParams" :key="index">
+            <h4>{{index}}</h4>
+            <p>{{name.toString()}}<p>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,17 +35,44 @@ export default {
   name: 'mqtt',
   data() {
     return {
-      startBtnStatus: false,
-      connectionStatus: false
+      mqttConnection: false,
+      client_server: false,
+      topicList: undefined,
+      topicParams:{},
+      subscribed: [],
+      hidden: false
     }
   },
   sockets: {
-    connect: function(){
-      console.log("socket connection has been established")
-      this.connectionStatus = true
+    browserConn: function(message){
+      // console.log("browserConn event sent by server")
+      this.client_server = message[0]
+      this.$socket.emit('brokerConn')
     },
-    startBtn: function(data){
-      this.startBtnStatus = data[0]
+    mqttConn: function(message){
+      // console.log("socket connection has been established")
+      this.mqttConnection = message[0]
+    },
+    topicData: function(data){
+      console.log(data)
+      this.topicParams = data
+    },
+    topicList: function(topics){
+      this.topicList = topics
+    }
+  },
+  methods: {
+    subTopic(topic){
+      console.log("Subscribing to this topic: ", topic)
+      this.$socket.emit('subTopic', topic)
+      this.subscribed.push(topic)
+    },
+    unsubTopic(topic){
+      this.$socket.emit('unSub', topic)
+      console.log("Unsubscribing from the topic: ", topic)
+    },
+    reConnect(){
+      this.$socket.emit('reConnect')
     }
   }
 }
@@ -29,6 +80,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#connections {
+  text-align: left;
+  font-size: 14px;
+  background: rgb(236, 236, 236);
+}
+
+.isConnected{
+  color: #42b983;
+}
+
+.notConnected{
+  color: rgb(214, 18, 18);
+}
+
+.machineParams{
+  font-size: 18px;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -39,8 +107,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
